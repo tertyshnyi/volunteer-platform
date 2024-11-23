84161bd4a76afd4346502517f9db759ea2c9383c
volodymyr.tertyshnyi <volodymyr.tertyshnyi@student.tuke.sk>
2024-11-23 12:58:38 +0100

    Updated architecture for the frontend.

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Home: React.FC = () => {
+const HomePage: React.FC = () => {
     return (
         <div>
             <h1>Welcome to Volunteer Platform</h1>
@@ -9,4 +9,4 @@ const Home: React.FC = () => {
     )
 }
 
-export default Home;
+export default HomePage;
